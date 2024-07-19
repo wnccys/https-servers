c6c097008a690eea3daafba92014a9cb5cd5553a
wnccys <ipooo346@gmail.com>
2024-07-19 19:24:12 -0300

    sets basic multi-thread handling

---
+++ run-test.sh
@@ -0,0 +1,3 @@
+(sleep 3 && printf "GET / HTTP/1.1\r\n\r\n") | nc localhost 4221 >>responses.txt &
+(sleep 3 && printf "GET / HTTP/1.1\r\n\r\n") | nc localhost 4221 >>responses.txt &
+(sleep 3 && printf "GET / HTTP/1.1\r\n\r\n") | nc localhost 4221 >>responses.txt &
