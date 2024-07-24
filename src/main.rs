bf172afee248c1bd6765b27020e49b76407863bc
wnccys <ipooo346@gmail.com>
2024-07-24 11:45:40 -0300

    fix: nseioqnseioqla

--- src/main.rs
+++ src/main.rs
@@ -158,10 +158,12 @@ fn handle_files(route: &str) -> String {
     let mut file_path = args[2].clone();
     file_path.push_str(file);
 
-    let current_dir = env::current_dir().unwrap();
-    let mut absolute_path = current_dir.to_str().unwrap().to_string();
+    // let current_dir = env::current_dir().unwrap();
+    let mut absolute_path = args[0].clone();
     absolute_path.push_str(&file_path);
-    dbg!(&absolute_path);
+    // let mut absolute_path = current_dir.to_str().unwrap().to_string();
+    // absolute_path.push_str(&file_path);
+    // dbg!(&absolute_path);
 
     let requested_file = fs::read_to_string(absolute_path);
     dbg!(&requested_file);
